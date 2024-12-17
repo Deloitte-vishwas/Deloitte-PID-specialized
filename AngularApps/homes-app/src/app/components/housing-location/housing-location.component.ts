38a14b1b03788e57d06b615ee971fe78f7568497
Vishwas K Singh <vishwasks32@gmail.com>
2024-12-17 17:05:47 +0530

    Using housing service

--- AngularApps/homes-app/src/app/components/housing-location/housing-location.component.ts
+++ AngularApps/homes-app/src/app/components/housing-location/housing-location.component.ts
@@ -1,10 +1,10 @@
 import { Component, Input } from '@angular/core';
 import { HousingLocation } from '../../interfaces/housing-location';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-housing-location',
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink],
   templateUrl: './housing-location.component.html',
   styleUrl: './housing-location.component.css'
 })
--- AngularApps/homes-app/src/app/pages/details/details.component.ts
+++ AngularApps/homes-app/src/app/pages/details/details.component.ts
@@ -1,6 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { HousingService } from '../../services/housing.service';
+import { HousingLocation } from '../../interfaces/housing-location';
 
 @Component({
   selector: 'app-details',
@@ -9,12 +11,16 @@ import { ActivatedRoute } from '@angular/router';
   styleUrl: './details.component.css'
 })
 export class DetailsComponent {
-
+  housingService: HousingService = inject(HousingService);
   route: ActivatedRoute = inject(ActivatedRoute);
-  housingLocationId = 0;
+  // housingLocationId = 0;
+  housingLocation: HousingLocation | undefined;
 
   constructor(){
-    this.housingLocationId = Number(this.route.snapshot.params['id']);
+    // this.housingLocationId = Number(this.route.snapshot.params['id']);
+    const housingLocationId = Number(this.route.snapshot.params['id']);
+    this.housingLocation = this.housingService.getHousingLocationById(housingLocationId);
+
   }
 
 }
