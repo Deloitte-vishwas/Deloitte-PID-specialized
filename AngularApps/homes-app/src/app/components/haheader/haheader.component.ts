250352314467f8e76f2b39c47f8dfe1bc3f4f678
Vishwas K Singh <vishwasks32@gmail.com>
2024-12-17 12:49:02 +0530

    homes-app initial

---
+++ AngularApps/homes-app/src/app/components/haheader/haheader.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-haheader',
+  imports: [],
+  templateUrl: './haheader.component.html',
+  styleUrl: './haheader.component.css'
+})
+export class HAHeaderComponent {
+
+}
